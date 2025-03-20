187d3d91defdca08f2378e5cbc73d500e39aa7e6
Philipi Sauer <philipisauer09@gmail.com>
2025-03-20 16:10:50 -0300

    exercicio05

---
+++ exercicio05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o valor do salario: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite o quantas vendas o funcionario fez: ");
+            double vendas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("quanto o funcionaro recebe de comissao por venda: ");
+            double comissao = Convert.ToDouble(Console.ReadLine());
+
+            double salarioFinal = salario + comissao * vendas;
+            Console.WriteLine($"o funcionario deverá receber: {salarioFinal}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
