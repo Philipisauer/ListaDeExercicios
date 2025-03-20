a7f51c8ddfad0d461effdb527ebaa1b20efb4031
Philipi Sauer <philipisauer09@gmail.com>
2025-03-20 13:56:31 -0300

    exercicio01 feito

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio01
+{
+    internal class Program
+    {
+        //calcular o volume de uma caixa retangular
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o comprimento da caixa");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("digite a largura da caixa");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("digite a altura da caixa");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+            decimal resultado = comprimento * largura * altura;
+
+            Console.WriteLine($"O volume da caixa retangular é: {resultado} cm ");
+
+            Console.ReadLine();
+        }
+    }
+}
