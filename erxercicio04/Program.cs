d496a5945cb6a0f14c55ca63ecb6f8156b509d0e
Philipi Sauer <philipisauer09@gmail.com>
2025-03-20 15:49:38 -0300

    exercicio04

---
+++ erxercicio04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace erxercicio04
+{
+    internal class Program
+    {
+        // FORMULA (F = C x 1,8 + 32)
+        //F=Fahrenheit
+        //C=Celsius
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite os Graus Celsius para transformar em fahreinheit: ");
+            double valor = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = valor * 1.8 + 32;
+
+            Console.WriteLine($"fahreinheit é igual a: {resultado}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
