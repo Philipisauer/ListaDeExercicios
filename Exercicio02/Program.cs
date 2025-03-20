98e24de985aa05449a1163e2845277fcdd276f88
Philipi Sauer <philipisauer09@gmail.com>
2025-03-20 14:25:00 -0300

    exercicio03 quase terminado

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        //calcular o volume de um cilindro
+        static void Main(string[] args)
+        {
+
+            double raio = 3.1;
+            double altura = 10;
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume é igual {volume} cm²");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        //calcular o consumo de gasolina
+        static void Main(string[] args)
+        {
+            double KmInicial = 500;
+            double KmFinal = 1000;
+
+            double ConsumoCombustivel = Convert.ToDouble(Console.ReadLine());
+
+            double distantiaPercorrida = KmFinal - KmInicial;
+
+            double ConsumoPorKm = distantiaPercorrida / ConsumoCombustivel;
+
+            Console.WriteLine($"O consumo aproximado foi de: {ConsumoPorKm} Km/L");
+
+            Console.ReadLine();
+        }
+    }
+}
