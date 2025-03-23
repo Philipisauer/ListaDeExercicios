aba041a176e9e17e88ddfebb2702b9c1cabb73f5
Philipi Sauer <philipisauer09@gmail.com>
2025-03-23 15:37:52 -0300

    exercicio09 terminado

---
+++ exercicio09/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicio09
+{
+    internal class Program
+    {
+        // Solicitar ao utilizador que insira o comprimento e a largura do terreno.
+        // 2.Calcular a área do terreno (área = comprimento * largura).
+        // 3. Exibir a área do terreno ao utilizador. 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o comprimento do terreno: ");
+            double comprimento = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite a largura do terreno: ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = comprimento * largura;
+
+            Console.WriteLine($"a area do terreno é {resultado}");
+            Console.ReadLine();
+        }
+    }
+}
