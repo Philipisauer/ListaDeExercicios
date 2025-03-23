3e19cedb3f7444497f70b10f6507ffe7c4adadf8
Philipi Sauer <philipisauer09@gmail.com>
2025-03-23 15:25:24 -0300

    exercicio08 finlizado

---
+++ exercicio08/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace exercicio08
+{
+    internal class Program
+    {
+        //numero é primo?
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("digite o numero desejado: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                bool numeroPrimo = true;
+
+                if (numero < 2)
+                    numeroPrimo = false;
+
+                if (numero == 2)
+                    numeroPrimo = true;
+
+                if (numero % 2 == 0)
+                    numeroPrimo = false;
+
+                for (int i = 3; i * i <= numero; i += 2)
+                {
+                    if (numero % i == 0)
+                        numeroPrimo = false;
+                }
+                    Console.WriteLine(numeroPrimo ? "numero primo" : "não é primo");
+                    Console.ReadLine();
+                
+            }
+        }
+    }
+}
+    
