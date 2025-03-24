c8222a14829ef2dd2d7d6e54736491ece163f3b1
Philipi Sauer <philipisauer09@gmail.com>
2025-03-24 15:42:40 -0300

    exercicio13 terminado

---
+++ exercicio13/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exercicio13
+{
+    internal class Program
+    {
+        //13. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor do lado A: ");
+            double ladoA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor do lado B: ");
+            double ladoB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor do lado C: ");
+            double ladoC= Convert.ToDouble(Console.ReadLine());
+
+            double soma = ladoA + ladoB;
+
+            if (soma < ladoC)
+
+                Console.WriteLine("A soma dos lados A e b é menor que o lado C.");
+
+            else
+
+                Console.WriteLine("a soma do lado A e lado B é maior que o lado C");
+            
+                Console.ReadLine();
+
+        }
+    }
+}
