d0bbfafcc969b05fb256b8c8386ebd8b9913f92f
Philipi Sauer <philipisauer09@gmail.com>
2025-03-24 15:19:05 -0300

    exercicio12 terminado

---
+++ exercicio12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exercicio12
+{
+    internal class Program
+    {
+        //Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%. Após o aumento, desconte 8% de
+        //impostos.Imprima o salário inicial, o salário com o aumento e o salário final.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o salario inicial do funcionario: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            double porcentagemDeAumento = salario * 0.15;
+
+            double salarioComAumento = salario + porcentagemDeAumento;
+
+            double porcentagemDeDesconto = salarioComAumento * 0.08;
+
+            double totalSalario = salarioComAumento - porcentagemDeDesconto;
+
+            Console.WriteLine($"salario inicial: {salario}");
+
+            Console.WriteLine($"salario com o aumento: {salarioComAumento}");
+
+            Console.WriteLine($"salario final: {totalSalario} (FAÇA O L)");
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
