15f373568d91b6e3371f9b3707a952752761d01e
Philipi Sauer <philipisauer09@gmail.com>
2025-03-24 14:29:03 -0300

    exercicio11 terminado

---
+++ exercicio11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //365 dias * anos = total de dias
+        {
+            Console.WriteLine("Digite seu nome ou o nome da pessoas a ser analisada:  ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Agora digite a idade: ");
+            double idade = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = idade * 365;
+
+            Console.WriteLine($"{nome} tem aproximadamente {resultado} dias de vida.");
+            Console.ReadLine();
+        }
+    }
+}
