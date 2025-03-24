d0758ac076f2e73df0538dab329110ab37fce159
Philipi Sauer <philipisauer09@gmail.com>
2025-03-24 14:12:07 -0300

    exercico10 resolvido

--- exercicio10/Program.cs
+++ exercicio10/Program.cs
@@ -5,11 +5,11 @@ namespace exercicio10
 {
     internal class Program
     {
-        //Ler quantidade_pães
-        // - Ler quantidade_broas
-        //3. Cálculo do total arrecadado
-        //// - Calcular total_pães = quantidade_pães * 0,12
-        //- Calcular total_broas = quantidade_broas * 1,50
+        //Ler quantidade_pães xx
+        // - Ler quantidade_broas xx
+        //3. Cálculo do total arrecadado xx
+        //// - Calcular total_pães = quantidade_pães * 0,12 xx
+        //- Calcular total_broas = quantidade_broas * 1,50 xx
         // - Calcular total_arrecadado = total_pães + total_broas
         //4. Cálculo do valor a guardar na poupança
         //  - Calcular valor_poupança = total_arrecadado * 0,10
@@ -20,17 +20,27 @@ namespace exercicio10
         {
 
 
-            Console.WriteLine("Insira o total de paes:  ");
+            Console.WriteLine("Insira o total de pães:  ");
             double paes = Convert.ToDouble(Console.ReadLine());
 
-            paes = 0,12;
+            paes = paes * 0.12;
 
             Console.WriteLine("Insira o total de broas:  ");
             double broas = Convert.ToDouble(Console.ReadLine());
 
-            broas = 1,50;
+            broas = broas * 1.50;
 
-            resultado = paes * 0,12 + broas * 1,50;
+            double resultado = paes + broas;
+
+            Console.WriteLine($"valor total de lucro é: {resultado}");
+            
+            double poupanca = resultado * 0.10;
+
+            Console.WriteLine($"o valor arrecadado para a poupança será de: {poupanca}");
+
+            Console.ReadLine();
+
+
         }
     }
 }
