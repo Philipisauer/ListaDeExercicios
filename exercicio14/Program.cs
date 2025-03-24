f3ebb75826f119ee2d37b9d57df3e1eb105ad2c1
Philipi Sauer <philipisauer09@gmail.com>
2025-03-24 16:15:51 -0300

    exercicio14 resolvido

---
+++ exercicio14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Design;
+
+namespace exercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro valor: ");
+            double valorUm = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo valor: ");
+            double valorDois = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite o terceiro valor: ");
+            double valorTres = Convert.ToDouble(Console.ReadLine());
+
+            if (valorUm > valorDois && valorUm > valorTres && valorDois > valorTres)
+
+                Console.WriteLine($"primeiro valor: {valorUm} segundo valor: {valorDois} terceiro valor: {valorTres}");
+
+            else if (valorUm < valorDois && valorUm < valorTres && valorDois < valorTres)
+
+                Console.WriteLine($"primeiro valor: {valorTres} segundo valor: {valorDois} terceiro valor: {valorUm}");
+
+            Console.ReadLine();
+        }
+    }
+}
