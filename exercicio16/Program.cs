f1f3c5b166f8017cb5ccfdcf87b984e2479bff4d
Philipi Sauer <philipisauer09@gmail.com>
2025-03-24 16:43:43 -0300

    exercicio16 terminado

---
+++ exercicio16/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o numero para descobrir se é par ou impar: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero % 2 == 0)
+                Console.WriteLine("o numero é par.");
+
+            else
+                Console.WriteLine("numero é impar.");
+
+            Console.ReadLine();
+        }
+    }
+}
